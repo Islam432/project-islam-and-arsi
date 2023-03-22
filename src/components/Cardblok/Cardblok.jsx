7c834a6c12b096a2150e9b8868a9ce8ebdceddbd
Islam Temirgaliev <islam2004tr@mail.ru>
2023-03-22 12:15:49 +0600

    forYou-commit

--- src/components/Cardblok/Cardblok.jsx
+++ src/components/Cardblok/Cardblok.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Cardblok.module.css";
 import { AiFillStar } from "react-icons/ai";
+import photo from './Photo.png'
+import Image from "next/image";
+// eslint-disable-next-line react-hooks/rules-of-hooks
 
 const Cardblok = () => {
-  return <div className={styles.card}>
-    <header className={styles.minHeder}>text</header>
-    <div className={styles.imgblok}>imgblog</div>
-    <h5 className={styles.name}>name</h5>
-    <footer className={styles.footer}>
+  const [number, setNumber] = useState([{id:1}, {id:2}, {id:3}, {id:4}, {id:5}]);
+  return (
+    <div className={styles.card}>
+      <header className={styles.minHeder}>text</header>
+ <Image alt="" className={styles.imgblok} src={photo}/>
+      <h5 className={styles.name}>name</h5>
+      <footer className={styles.footer_pz}>
         <h4 className={styles.price}>price</h4>
         <div className={styles.starblok}>
+          {number.map((item) => {
+            return <AiFillStar className={styles.star} key={item.id}/>;
+          })}
+          {/* <AiFillStar className={styles.star}/>
             <AiFillStar className={styles.star}/>
             <AiFillStar className={styles.star}/>
             <AiFillStar className={styles.star}/>
             <AiFillStar className={styles.star}/>
-            <AiFillStar className={styles.star}/>
-           
+            */}
         </div>
-    </footer>
-  </div>;
+      </footer>
+    </div>
+  );
 };
 
 export default Cardblok;
