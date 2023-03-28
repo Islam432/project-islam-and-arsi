ff74c2ca1dcb04d48d6130c7d73f6d6be3a9084d
Islam Temirgaliev <islam2004tr@mail.ru>
2023-03-28 09:27:59 +0600

    commit dataBase

---
+++ src/components/Api/restApi.js
@@ -0,0 +1,20 @@
+import ky from "ky";
+import * as qs from "qs";
+
+const strapi = ky.create({ prefixUrl: process.env.NEXT_PUBLIC_STRAPI_API });
+
+export const dataStrapi = () => {
+  const query = qs.stringify({
+    populate: {
+      Picture: true,
+    },
+  });
+  return strapi.get("catalogs?" + query).json();
+};
+export const SevenData = () => {
+    const query = qs.stringify({
+        page: 1,
+        pageSize: 8,
+    })
+    return strapi.get("catalogs?" + query).json();
+}
