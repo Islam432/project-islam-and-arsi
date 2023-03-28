38fde7a4a661cb8907510259919b5a5ca2018aaa
Arsi chan <usupovarslan56@gmail.com>
2023-03-28 09:45:12 +0600

    Arsi_commit4

---
+++ src/components/Grids/Grids.jsx
@@ -0,0 +1,22 @@
+import Styles from "./Grids.module.css"
+
+const Grids = () => {
+    return (
+    <div className={Styles.main_Grids}>
+        <div className={Styles.grid_container}>
+            <div className={Styles.first_grid}>
+
+            </div>
+            <div className={Styles.second_grid}>
+
+            </div>
+            <div className={Styles.three_grid}>
+
+            </div>
+        </div>
+    </div>
+  
+    )
+  }
+  
+  export default Grids
